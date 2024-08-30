9713061e58549c205b3e73e4a888778c557fcf90
Mahesh Vashisth <maheshvashisth07@gmail.com>
2024-08-30 19:26:26 +0530

    updated processor

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,45 +1,48 @@
+// processor take the data from db second table and put it into the kafka queue
 import { PrismaClient } from "@prisma/client";
-import {Kafka} from "kafkajs";
+import { Kafka } from "kafkajs";
 
-const TOPIC_NAME = "zap-events"
+const TOPIC_NAME = "zap-events";
 
 const client = new PrismaClient();
 
 const kafka = new Kafka({
-    clientId: 'outbox-processor',
-    brokers: ['localhost:9092']
-})
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
 
 async function main() {
-    const producer =  kafka.producer();
-    await producer.connect();
+  const producer = kafka.producer();
+  await producer.connect();
 
-    while(1) {
-        const pendingRows = await client.zapRunOutbox.findMany({
-            where :{},
-            take: 10
-        })
-        console.log(pendingRows);
+  //  zaprun and outbox run together
+  while (1) {
+    const pendingRows = await client.zapRunOutbox.findMany({
+      where: {},
+      take: 10,
+    });
+    console.log(pendingRows);
 
-        producer.send({
-            topic: TOPIC_NAME,
-            messages: pendingRows.map(r => {
-                return {
-                    value: JSON.stringify({ zapRunId: r.zapRunId, stage: 0 })
-                }
-            })
-        })  
+    // pendingRows is an array
+    producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingRows.map((r) => {
+        return {
+          value: JSON.stringify({ zapRunId: r.zapRunId, stage: 0 }),
+        };
+      }),
+    });
 
-        await client.zapRunOutbox.deleteMany({
-            where: {
-                id: {
-                    in: pendingRows.map(x => x.id)
-                }
-            }
-        })
+    await client.zapRunOutbox.deleteMany({
+      where: {
+        id: {
+          in: pendingRows.map((x) => x.id),
+        },
+      },
+    });
 
-        await new Promise(r => setTimeout(r, 3000));
-    }
+    await new Promise((r) => setTimeout(r, 3000));
+  }
 }
 
 main();
