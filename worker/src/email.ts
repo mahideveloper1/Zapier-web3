f505afc7bb7f776d9b230f5e617d5b84eacbe35c
Mahesh Vashisth <maheshvashisth07@gmail.com>
2024-09-02 23:39:47 +0530

    updated email

--- worker/src/email.ts
+++ worker/src/email.ts
@@ -1,25 +1,21 @@
 import nodemailer from "nodemailer";
-// SOL_PRIVATE_KEY=""
-// SMTP_USERNAME=""
-// SMTP_PASSWORD=""
-// SMTP_ENDPOINT
 
 const transport = nodemailer.createTransport({
-    host: process.env.SMTP_ENDPOINT,
-    port: 587,
-    secure: false, // upgrade later with STARTTLS
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
+  host: process.env.SMTP_ENDPOINT,
+  port: 587,
+  secure: false, // upgrade later with STARTTLS
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
 
 export async function sendEmail(to: string, body: string) {
-    await transport.sendMail({
-        from: "contact@100xdevs.com",
-        sender: "contact@100xdevs.com",
-        to,
-        subject: "Hello from Zapier",
-        text: body
-    })
+  await transport.sendMail({
+    from: "maheshvashisth1111@gmail.com",
+    sender: "maheshvashisth1111@gmail.com",
+    to,
+    subject: "Hello from Zapier",
+    text: body,
+  });
 }
